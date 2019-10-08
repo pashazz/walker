ab7eb484121e165092e1d7955ccfe14bcea2ce98
Pavel Zinin <pzinin@gmail.com>
2019-10-08 13:05:26 +0300

    Initial

---
+++ src/main/java/io/github/pashazz/walker/WalkerApplication.java
@@ -0,0 +1,13 @@
+package io.github.pashazz.walker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WalkerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WalkerApplication.class, args);
+	}
+
+}
